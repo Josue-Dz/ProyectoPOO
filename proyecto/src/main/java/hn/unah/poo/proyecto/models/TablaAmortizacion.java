b241b99e109fd35179b0f487d15986389c6bcd4c
Eduardo Gabriel Martinez <eduardo.martinez@unah.hn>
2024-12-01 22:51:20 -0600

    Update TablaAmortizacion.java
    Se agrego comentarios a las variables.

--- proyecto/src/main/java/hn/unah/poo/proyecto/models/TablaAmortizacion.java
+++ proyecto/src/main/java/hn/unah/poo/proyecto/models/TablaAmortizacion.java
@@ -23,24 +23,43 @@ import lombok.Setter;
 @Entity
 @Table(name="amortizacion")
 public class TablaAmortizacion {
-
+  /**
+     * Id de la tabla Embebida; Creada para generar un primary key compuesto de
+     * (numerocuota y idPrestamo)
+     */
     @EmbeddedId
     private TablaAmortizacionId id;
-
+ /**
+     * Interes pagado por el cliente en tablaAmortizacion
+     */
     @Column(columnDefinition = "DECIMAL(14,2)")
     private double interes;
-
+    /**
+     * Capital pagado por el cliente en tablaAmortizacion
+     */
     @Column(columnDefinition = "DECIMAL(14,2)")
     private double capital;
-
+    /**
+     * Saldo a pagar por el cliente en tablaAmortizacion
+     */
     @Column(columnDefinition = "DECIMAL(14,2)")
     private double saldo;
-
+    /**
+     * Estado de la cuota en tablaAmortizacion
+     */
     private char estado;
-
+    /**
+     * Fecha a vencer para pagar la cuota en tablaAmortizacion
+     */
     @Column(name = "fechavencimiento")
-    private  LocalDate fechaVencimiento;
+    private LocalDate fechaVencimiento;
 
+
+    // Usamos insertable=false, updatable=false para evitar la duplicación de la
+    // columna idprestamo
+    /**
+     * Objeto que obtiene toda la informacion del prestamo
+     */
     @ManyToOne
     @JoinColumn(name = "idprestamo", referencedColumnName= "idprestamo", insertable = false, updatable = false)
     @JsonIgnore
