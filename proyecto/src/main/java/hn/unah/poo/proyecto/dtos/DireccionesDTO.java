4663d45dc64b82358d10bbb535e74a150e2ac8a2
Eduardo Gabriel Martinez <eduardo.martinez@unah.hn>
2024-12-01 22:47:52 -0600

    Update DireccionesDTO.java
    Se agrego comentarios a las variables y tambien se creo la variable clienteDTO para la relacion bidireccional.

--- proyecto/src/main/java/hn/unah/poo/proyecto/dtos/DireccionesDTO.java
+++ proyecto/src/main/java/hn/unah/poo/proyecto/dtos/DireccionesDTO.java
@@ -1,5 +1,7 @@
 package hn.unah.poo.proyecto.dtos;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,17 +12,35 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class DireccionesDTO {
-    
+    /**
+     * Id de la clase DireccionDTO
+     */
     private int idDirecciones;
 
+    /**
+     * Pais del clienteDTO
+     */
     private String pais;
-    
+    /**
+     * Departamento del clienteDTO
+     */
     private String departamento;
-
+    /**
+     * Ciudad del clienteDTO
+     */
     private String ciudad;
-
+    /**
+     * Colonia del clienteDTO
+     */
     private String colonia;
-
+    /**
+     * Referencia de donde vive el clienteDTO
+     */
     private String referencia;
-
+    /**
+     * Objeto clienteDTO que contiene la informacion del mismo en la clase
+     * DireccionDTO.
+     */
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private ClienteDTO clienteDTO;
 }
